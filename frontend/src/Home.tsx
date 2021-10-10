cb584e595f50e5b197647074b45e21e423c667cc
olets123 <47143886+olets123@users.noreply.github.com>
2021-10-10 19:06:26 +0200

    Fix/paypal (#1)
    * install react charts
    
    * paypal client id
    
    * fix chart margin and design

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -9,10 +9,11 @@ import Typography from "@mui/material/Typography";
 export const Home: React.FC<{}> = (props) => {
   const theme = useTheme();
   const checkIfMobile = useMediaQuery("(min-width: 600px)");
+
   return (
     <Container>
       <Box display="flex" justifyContent="center" width="100%">
-        <Grid container spacing={2}>
+        <Grid container>
           <Grid item xs={12}>
             <BoujeeChart />
           </Grid>
@@ -23,36 +24,72 @@ export const Home: React.FC<{}> = (props) => {
                 elevation={3}
                 sx={{
                   display: "flex",
+                  flexDirection: "column",
                   justifyContent: "center",
                   padding: theme.spacing(2),
-                  margin: theme.spacing(2),
+                  margin: theme.spacing(0),
                   width: "100%",
                   maxWidth: checkIfMobile ? theme.breakpoints.values.sm : 375,
                   background: theme.palette.primary.main,
                 }}
               >
-                <Typography variant="subtitle1">Test Text</Typography>
+                <PayPal />
               </Paper>
             </Box>
           </Grid>
           <Grid item xs={12}>
             <Box display="flex" justifyContent="center" m={0} width="100%">
-              {/* <Paper
+              <Paper
                 variant="elevation"
                 elevation={3}
                 sx={{
                   display: "flex",
+                  flexDirection: "column",
                   justifyContent: "center",
                   padding: theme.spacing(2),
-                  margin: theme.spacing(2),
-
+                  marginTop: theme.spacing(2),
+                  marginBottom: theme.spacing(2),
                   width: "100%",
                   maxWidth: checkIfMobile ? theme.breakpoints.values.sm : 375,
                   background: theme.palette.primary.main,
                 }}
-              > */}
-              <PayPal />
-              {/*   </Paper> */}
+              >
+                <Typography
+                  variant="subtitle1"
+                  sx={{ paddingBottom: theme.spacing(2) }}
+                >
+                  In this VIP group you will get inside info bets by{" "}
+                  <strong>BoujeeSB</strong> and <strong>BigBrainSB</strong>.
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  sx={{ paddingBottom: theme.spacing(2) }}
+                >
+                  You will get a notification 3 minutes before we post each bet,
+                  so you have time to set up and be ready to place the bet.
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  sx={{ paddingBottom: theme.spacing(2) }}
+                >
+                  Using a 1-5 unit-scale, to secure a solid and trustful
+                  service. We’re not spamming high unit bets as other services
+                  do…
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  sx={{ paddingBottom: theme.spacing(2) }}
+                >
+                  Good bookies to have: Bet365, Pinnacle, Kambi. We post all our
+                  bets in a Telegram group.
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  sx={{ paddingBottom: theme.spacing(2) }}
+                >
+                  We post all our bets in a Telegram group.
+                </Typography>
+              </Paper>
             </Box>
           </Grid>
         </Grid>
