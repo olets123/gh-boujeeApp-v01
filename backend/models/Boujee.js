282631437575bd2de8fa33b6094a52c05908bce5
Ole Thomas Skogli <skogliiii@gmail.com>
2022-01-29 17:26:13 +0100

    add year

--- backend/models/Boujee.js
+++ backend/models/Boujee.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const BoujeeSchema = new mongoose.Schema({
+  year: {
+    type: String,
+    required: true,
+  },
   month: {
     type: String,
     required: true,
